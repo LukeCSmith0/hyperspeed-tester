f9ed01c3dfdf4f654df517fae35ca21c6e15a889
Luke Smith <30316540+LukeCSmith0@users.noreply.github.com>
2017-07-24 11:51:06 +0100

    Update iperf-server-install-script.sh

--- Installation-Scripts/iperf-server-install-script.sh
+++ Installation-Scripts/iperf-server-install-script.sh
@@ -5,5 +5,8 @@ apt-get update
 echo *******Installing IPerf3*******
 sleep 2
 apt-get install iperf3 -y
+echo *******Installing RSSH for RCP Pulls*******
+sleep 2
+apt-get rssh -y
 echo *******Setup complete*******
 sleep 2
