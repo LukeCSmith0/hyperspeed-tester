4d8914e2e96f6122e8ee9a62b4bceff735dcb37b
Luke Smith <30316540+LukeCSmith0@users.noreply.github.com>
2017-07-28 14:50:15 +0100

    Update iperf-log-server-install-script.sh

--- Installation-Scripts/iperf-log-server-install-script.sh
+++ Installation-Scripts/iperf-log-server-install-script.sh
@@ -1,5 +1,8 @@
 echo *******Installing RSSH for RCP Pulls*******
 sleep 2
 apt-get install rssh -y
+echo ******Copying scripts*******
+wget -O /home/iperf-scripts/json_server_side.py https://raw.githubusercontent.com/LukeCSmith0/hyperspeed-tester/master/Server-Script/json_server_side.py
+wget -O /home/iperf-scripts/switch_information_check.py https://raw.githubusercontent.com/LukeCSmith0/hyperspeed-tester/master/Server-Script/switch_information_check.py
 echo *******Setup complete*******
 sleep 2
