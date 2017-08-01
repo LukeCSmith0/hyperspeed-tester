db94f74dec8f388cc4a01c5775cced775e8fa151
Luke Smith <30316540+LukeCSmith0@users.noreply.github.com>
2017-08-01 09:37:58 +0100

    Added in directory creation
    Adds in the directories that are used by default for the scripts, and log storage.

--- Installation-Scripts/iperf-log-server-install-script.sh
+++ Installation-Scripts/iperf-log-server-install-script.sh
@@ -4,7 +4,13 @@ apt-get update
 echo *******Installing RSSH for RCP Pulls*******
 sleep 2
 apt-get install rssh -y
+echo ******Creating directories required for logs*******
+sleep 2
+mkdir /home/iperf-scripts
+mkdir /home/iperf
+mkdir /home/test-logs
 echo ******Copying scripts*******
+sleep 2
 wget -O /home/iperf-scripts/json_server_side.py https://raw.githubusercontent.com/LukeCSmith0/hyperspeed-tester/master/Server-Script/json_server_side.py
 wget -O /home/iperf-scripts/switch_information_check.py https://raw.githubusercontent.com/LukeCSmith0/hyperspeed-tester/master/Server-Script/switch_information_check.py
 echo *******Setup complete*******
