bd48b1edc59777c6fac8563f58025af3ea09d067
Luke Smith <30316540+LukeCSmith0@users.noreply.github.com>
2017-08-01 09:14:25 +0100

    Added Flask install to script

--- Installation-Scripts/iperf-server-install-script.sh
+++ Installation-Scripts/iperf-server-install-script.sh
@@ -8,5 +8,11 @@ apt-get install iperf3 -y
 echo *******Installing RSSH for RCP Pulls*******
 sleep 2
 apt-get install rssh -y
+echo *******Installing PIP for Flask Installation*******
+sleep 2
+apt-get install python-pip
+echo *******Installing Flask*******
+sleep 2
+pip install flask
 echo *******Setup complete*******
 sleep 2
