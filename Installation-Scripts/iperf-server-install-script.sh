537c38969874c7349e81b66de00f5b12b4a698c6
Luke Smith <30316540+LukeCSmith0@users.noreply.github.com>
2017-08-08 14:29:34 +0100

    Include new WSGI scripts for server installation

--- Installation-Scripts/iperf-server-install-script.sh
+++ Installation-Scripts/iperf-server-install-script.sh
@@ -10,13 +10,24 @@ sleep 2
 apt-get install rssh -y
 echo *******Installing PIP for Flask Installation*******
 sleep 2
-apt-get install python-pip
+apt-get install python-pip -y
 echo *******Installing Flask*******
 sleep 2
 pip install flask
+echo *******Installing WSGI for the Flask application*******
+sleep 2
+apt-get install libapache2-mod-wsgi -y
 echo *******Creating the required directories for scripts and pulling scripts*******
 sleep 2
 mkdir /home/whats-my-ip
-wget -O /home/whats-my-ip https://raw.githubusercontent.com/LukeCSmith0/hyperspeed-tester/master/Server-Script/whats-my-ip/whats-my-ip.py
+wget -O /home/whats_my_ip.py https://raw.githubusercontent.com/LukeCSmith0/hyperspeed-tester/master/Server-Script/whats-my-ip/whats_my_ip.py
+wget -O /home/whats-my-ip.wsgi https://raw.githubusercontent.com/LukeCSmith0/hyperspeed-tester/master/Server-Script/whats-my-ip.wsgi
+wget -O /etc/apache2/sites-available/whats-my-ip.conf https://raw.githubusercontent.com/LukeCSmith0/hyperspeed-tester/master/Server-Script/whats-my-ip.conf
+echo *******Registering App with Apache*******
+sleep 2
+a2ensite whats-my-ip
+echo *******Restarting Apache*******
+sleep 2
+/etc/init.d/apache2 restart
 echo *******Setup complete*******
 sleep 2
