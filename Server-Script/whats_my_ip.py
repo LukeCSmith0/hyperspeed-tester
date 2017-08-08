1fc0015f0dc02cb024ad79f52b315c0428ae0e80
Luke Smith <30316540+LukeCSmith0@users.noreply.github.com>
2017-08-08 14:24:49 +0100

    Added in logging to a file for the script

--- Server-Script/whats_my_ip.py
+++ Server-Script/whats_my_ip.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import logging
+from logging.handlers import RotatingFileHandler
+#from logging import Formatter
 
 app = Flask(__name__)
-@app.route("/whats-my-ip")
+@app.route("/")
 def main():
-	return jsonify({"ip" : request.remote_addr}), 200
+        log = logging.getLogger('werkzeug')
+        log.setLevel(logging.WARNING)
+        handler = logging.handlers.RotatingFileHandler(
+        '/home/whats-my-ip/logs.log'
+        )
+        log.addHandler(handler)
+        return jsonify({"ip" : request.remote_addr}), 200
+        return "Chickens"
 if __name__ == "__main__":
-	app.run(host="X.X.X.X", port="Y")
+        app.run(host="X.X.X.X", port="Y")
